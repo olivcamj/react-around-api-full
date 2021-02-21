6077417824fa08d7ea91e44ba8edb89ecfcf5e0e
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-21 18:09:09 -0500

    refactor: add options obj from deprecation warning
    Prevent future issues: this version is in the procress of being replaced

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 //  Implementing a temporary authorization solution

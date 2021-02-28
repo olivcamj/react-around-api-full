50a096d035bb730aed5b699476588451c8f9a7c8
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-27 19:37:39 -0500

    feat: add errors

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,30 +1,31 @@
 const Cards = require('../models/card');
-const NotFoundError = require('../errors/not-found-err.js');
-const BadRequestError = require('../errors/bad-request-err.js');
-const ForbiddenError = require('../errors/forbidden-err.js')
 
-const getCards = (req, res, next) => {
+const getCards = (req, res) => {
   Cards.find({})
     .then((cards) => {
       res.status(200).send(cards);
     })
-    .catch(next);
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
 };
 
-const createCard = (req, res, next) => {
+const createCard = (req, res) => {
   const { name, link } = req.body;
 
   Cards.create({ name, link, owner: req.user._id })
     .then((card) => {
       if (!card) {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: 'Invalid data' });
       }
       res.status(200).send(card);
     })
-    .catch(next);
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
 };
 
-const likeCard = (req, res, next) => {
+const likeCard = (req, res) => {
   Cards.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
@@ -32,20 +33,20 @@ const likeCard = (req, res, next) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Card not found');
+        res.status(404).send({ message: 'Card not found' });
       }
       res.status(200).send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: err });
       }
-      next(err);
-    })
-    .catch(next);
+    });
 };
 
-const dislikeCard = (req, res, next) => {
+const dislikeCard = (req, res) => {
   Cards.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // remove _id from the array
@@ -53,37 +54,33 @@ const dislikeCard = (req, res, next) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Card not found');
+        res.status(404).send({ message: 'Card not found' });
       }
       res.status(200).send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: err });
       }
-      next(err);
-    })
-    .catch(next);
+    });
 };
 
-const deleteCard = (req, res, next) => {
+const deleteCard = (req, res) => {
   Cards.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      if (card) {
-        res.status(200).send(card);
-      } else if (!card) {
-        throw new NotFoundError('Card not found');
-      } else {
-        throw new ForbiddenError('You can only delete your own cards.');
+      if (!card) {
+        res.status(404).send({ message: 'No card found' });
       }
+      res.status(200).send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: 'Bad Request' });
       }
-      next(err);
-    })
-    .catch(next);
+      res.status(500).send({ message: 'Internal Server Error' });
+    });
 };
 
 module.exports = {
--- backend/errors/unauthorized-err.js
+++
@@ -1,8 +0,0 @@
-class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
-
-module.exports = UnauthorizedError;

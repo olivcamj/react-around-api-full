53a42cc9739369b408e325e02c1e64f042647d44
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-27 19:40:14 -0500

    feat: create and login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,96 +1,55 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const User = require('../models/user');
-const NotFoundError = require('../errors/not-found-err.js');
-const BadRequestError = require('../errors/bad-request-err.js');
-const UnauthorizedError = require('../errors/unauthorized-err.js');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-const SALT_ROUNDS = 10;
-
-const getUsers = (req, res, next) => {
+const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
       res.status(200).send(users);
     })
-    .catch(next);
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 };
 
-const getOneUser = (req, res, next) => {
-  User.findById(req.user._id)
+const getOneUser = (req, res) => {
+  // Check if the User Id exists
+  const isValidId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!isValidId) {
+    res.status(404).send({ message: 'User Not Found - invalid id' });
+  }
+
+  User.findById(req.params.id)
     .then((user) => {
-      console.log(user);
       if (!user) {
-        throw new NotFoundError('No user with matching ID found');
+        res.status(404).send({ message: 'User ID not found' });
       }
       res.status(200).send(user);
     })
-    .catch(next);
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: err });
+      }
+    });
 };
 
 const createUser = (req, res) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
-
-  if (!password || !email) return res.status(400).send({ message: 'Email and password fields should not be empty' });
-
-  console.log('1st createUser: ', email, password);
-
-  return bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
-    console.log('2nd!!! createUser: ', email, password);
-
-    return User.findOne({ email }).select('+password')
-      .then((userExists) => {
-        if (userExists) return res.status(403).send({ message: 'a user with this email already exists' });
-
-        return User.create(
-          {
-            name,
-            about,
-            avatar,
-            email,
-            password: hash,
-          },
-        )
-          .then((user) => res.send({ id: user._id, email: user.email }))
-          .catch(() => {
-            if (err.name === 'CastError') {
-              throw new BadRequestError('User cannot be created'); // message: 'User cannot be created
-            }
-          });
-      });
-  });
-};
-
-const login = (req, res, next) => {
-  const { email, password } = req.body;
-
-  return User.findUserByCredentials(email, password)
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+  // returns the recorded data
     .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Incorrect password or email');
-      }
-      // authentication successful! user is in the user variable
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'practicum',
-        { expiresIn: '7d' },
-      );
-      res.cookie('token', token, { httpOnly: true });
-      res.send({ token });
+      res.status(200).send(user);
     })
     .catch((err) => {
-      // authentication error
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err }); // 'Error: data not recorded'
       }
-      next(err);
-    })
-    .catch(next);
+      res.status(500).send({ message: err });
+    });
 };
 
-const updateProfile = (req, res, next) => {
+const updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
     req.params._id,
@@ -106,14 +65,14 @@ const updateProfile = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: err });
       }
-      next(err);
-    })
-    .catch(next);
+    });
 };
 
-const updateAvatar = (req, res, next) => {
+const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(
     req.params._id,
@@ -129,18 +88,17 @@ const updateAvatar = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Invalid data');
+        res.status(400).send({ message: err });
+      } else {
+        res.status(500).send({ message: err });
       }
-      next(err);
-    })
-    .catch(next);
+    });
 };
 
 module.exports = {
   getUsers,
   getOneUser,
   createUser,
-  login,
   updateProfile,
   updateAvatar,
 };

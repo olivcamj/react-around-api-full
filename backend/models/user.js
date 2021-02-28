04e90fcfbfcbc226e6cd2a35c525b767351b7ffd
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-27 19:39:27 -0500

    feat: new fields and a method

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,66 +1,27 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); // importing bcrypt
 const validator = require('validator');
-const UnauthorizedError = require('../errors/unauthorized-err.js');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'Jacques Cousteau',
   },
   about: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'Explorer',
   },
   avatar: {
     type: String,
-    validate: {
-      validator: (v) => validator.isURL(v, [
-        {
-          protocols: ['http', 'https', 'ftp'],
-          require_tld: true,
-          require_protocol: true,
-        },
-      ]),
-      message: "Sorry. The 'avatar' field must have a valid URL.",
-    },
-    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
-  },
-  email: {
-    type: String,
     required: true,
-    unique: true,
     validate: {
-      validator: (v) => validator.isEmail(v),
-      message: "Sorry. The 'email' field must have a email address",
+      validator: (v) => validator.isURL(v, [{ protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true }]),
+      message: 'Sorry. The \'avatar\' field must have a valid URL.',
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    select: false,
   },
 });
-
-userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Incorrect email or password');
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError('Incorrect email or password');
-          }
-          return user; // now user is available
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);

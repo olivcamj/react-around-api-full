018e4495ca3df862d5688880f82948d5aa252783
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-27 19:38:43 -0500

    feat: validate inbound server data

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards,
@@ -9,45 +8,10 @@ const {
   dislikeCard,
 } = require('../controllers/cards.js');
 
-router.get('/cards', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
-  }).options({ allowUnknown: true }),
-}), getCards);
-
-router.post('/cards', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
-  }).options({ allowUnknown: true }),
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(/(https?:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(:?\d*)\/?([a-z_/0-9\-#.]*)\??([a-z_/0-9\-#=&]*)/),
-    likes: Joi.array().items(Joi.string()),
-  }),
-}), createCard);
-
-router.put('/cards/:cardId/likes', celebrate({
-  headers: Joi.object().keys({
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
-}), likeCard);
-
-router.delete('/cards/:cardId/likes', celebrate({
-  headers: Joi.object().keys({
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
-}), dislikeCard);
-
-router.delete('/cards/:cardId', celebrate({
-  headers: Joi.object().keys({
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
-}), deleteCard);
+router.get('/cards', getCards);
+router.post('/cards', createCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
+router.delete('/cards/:cardId', deleteCard);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,44 +1,14 @@
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
 
 const router = express.Router();
 const {
-  getUsers, getOneUser, updateProfile, updateAvatar,
+  getUsers, getOneUser, createUser, updateProfile, updateAvatar,
 } = require('../controllers/users.js');
 
-router.get('/users', celebrate({
-  body: Joi.object().keys({
-    title: Joi.string().required().min(2).max(30),
-    text: Joi.string().required().min(2),
-  }),
-}), getUsers);
-
-router.get('/users/find/:id', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
-  }).options({ allowUnknown: true }),
-  params: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
-  }),
-}), getOneUser);
-
-router.get('users/me', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().regex(/^(Bearer )[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/).required(),
-  }).options({ allowUnknown: true }),
-}), getOneUser);
-
-router.patch('/users/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-}), updateProfile);
-
-router.patch('/users/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string(),
-  }),
-}), updateAvatar);
+router.get('/users', getUsers);
+router.get('/users/:id', getOneUser);
+router.post('/users', createUser);
+router.patch('/users/me', updateProfile);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;

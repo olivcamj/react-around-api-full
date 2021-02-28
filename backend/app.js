db5f18f52089e1a96d73a9138d7bfaf025868a9f
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-02-28 00:51:37 -0500

    fix: set max

--- backend/app.js
+++ backend/app.js
@@ -42,8 +42,8 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
-      name: Joi.string().min(2).min(30),
-      about: Joi.string().min(2).min(30),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
       avatar: Joi.string(),
     }),
   }),
